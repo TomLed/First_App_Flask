85f3219c9fc3f2ac2032a2a8cf5cb3c5b69f5d58
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-07 13:46:44 +0100

    new tests

---
+++ tests/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+
+from app import create_app
+
+from flask import template_rendered
+
+
+@pytest.fixture
+def user_name():
+    return "adrien"
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    return app
+
+
+@pytest.fixture
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,34 @@
 from flask import Flask
 
-from app import app
-
-def test_app():
+def test_app(app):
     assert app is not None
     assert isinstance(app, Flask)
 
-def test_user_template():
-    client = app.test_client()
-    response = client.get("/user/adrien")
-    template = app.jinja_env.get_template('user.html')
-    assert template.render(name="adrien") == response.get_data(as_text=True)
 
+def test_index_route(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "ECM" in response.get_data(as_text=True)
+
+def test_index_user(client, user_name):
+    response = client.get(f"/user/{user_name}")
+    assert response.status_code == 200
+    assert f"Hello {user_name}" in response.get_data(as_text=True)
+
+
+def test_user_template(app, client, user_name):
+    response = client.get(f"/user/{user_name}")
+    template = app.jinja_env.get_template('user.html')
+    assert template.render(name=user_name) == response.get_data(as_text=True)
+
+
+def test_user_view_uses_correct_template(client, captured_templates, user_name):
+    response = client.get(f"/user/{user_name}")
+    assert len(captured_templates) == 1
+
+    template, context = captured_templates[0]
+
+    assert template.name == "user.html"
+
+    assert "name" in context
+    assert context["name"] == user_name
