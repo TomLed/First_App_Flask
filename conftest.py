ba5cd3534e7028d445eab8fdd59ae42573b34286
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-08 15:50:03 +0100

    Adding enough to use it with docker

---
+++ conftest.py
@@ -0,0 +1,46 @@
+import pytest
+
+from flask import template_rendered
+from flask_sqlalchemy import SQLAlchemy
+
+
+from app import create_app, db
+
+
+@pytest.fixture
+def user_name():
+    return "adrien"
+
+
+@pytest.fixture
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+
+@pytest.fixture(scope='session')
+def app(request):
+    app = create_app()
+    # Establish an application context before running the tests.
+    ctx = app.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
+    return app
+
+
+@pytest.fixture(scope='session')
+def _db(app):
+    db.create_all()
+    return db
